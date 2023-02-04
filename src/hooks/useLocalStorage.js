928a91bb346989ee9ef196c4be15a95fb8e3a5d2
sandeepmallina <mallinasandeep21@gmail.com>
2023-02-04 22:15:11 +0530

    expense tracker first commit

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import React from 'react'
+
+function useLocalStorage(key,initialValue) {
+    const [value,setValue]=useState(()=>{
+      const jsonValue=localStorage.getItem(key)
+      if(jsonValue !=null){
+        return JSON.parse(jsonValue)
+      }
+    if(typeof initialValue === "function"){
+        return initialValue();
+    }
+    return initialValue;
+    })
+    useEffect(()=>{
+    localStorage.setItem(key,JSON.stringify(value))
+    },[key,value])
+  return [value,setValue];
+}
+
+export default useLocalStorage
